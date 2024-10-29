d42319d40a7f1da88603085e90199ed7374e1ceb
Wasif2000 <wasifali13579000@gmail.com>
2024-10-29 10:02:30 +0500

    new line add

--- src/pages/Approach.jsx
+++ src/pages/Approach.jsx
@@ -3,26 +3,25 @@ import React from 'react'
 function Approach() {
   return (
     <div className='bg-[#cdea68] w-full rounded-lg'>
-       <div className='p-14  pl-20'> <h1 className='text-black text-[2.5rem] font-bold'>Ochi is a strategic presentation agency for forward-thinking businesses that need to <span className='underline'> raise funds, sell prod­ucts, ex­plain com­plex ideas,</span> and <span className='underline'> hire great peo­ple.</span></h1></div>
+       <div className='p-14  pl-20'> <h1 className='text-black text-[2.3rem] font-bold'>Ochi is a strategic presentation agency for forward-thinking businesses that need to <span className='underline'> raise funds, sell prod­ucts, ex­plain com­plex ideas,</span> and <span className='underline'> hire great peo­ple.</span></h1></div>
 
-        <div className='border border-zinc-900 border-t border-b flex'>
-            <div className='border border-black w-[50%] p-8'>
+        <div className='border-t-[1px] border-zinc-800 flex'>
+            <div className=' w-[50%] pl-[75px] pt-7 '>
                 <p>What you can expect:</p>
             </div>
-            <div className='border border-black w-[35%] pl-8 pt-20 pb-20'>
+            <div className=' w-[35%] pl-8 pt-7 pb-20 pr-24 '>
                 <p>We create tailored presentations to help you persuade your colleagues, clients, or investors. Whether it’s live or digital, delivered for one or a hundred people.</p>
                 <br />
-                <br />
-                <br />
+                < br />
                 <p>We believe the mix of strategy and design (with a bit of coffee) is what makes your message clear, convincing, and captivating.</p>
             </div>
-            <div className='border border-black w-[15%] pl-6 pt-[140px] underline  leading-none'>
+            <div className=' w-[15%] pl-6 pt-[100px] underline  leading-tight'>
                 <p>S:</p>
                 <br />
-                <br />
+              
                 <br />
                 <p>Instagram <br />
-Behance <br />
+Binance <br />
 Facebook <br />
 Linkedin</p>
             </div>
