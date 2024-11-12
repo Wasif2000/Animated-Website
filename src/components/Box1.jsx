38b18e1ec03ba519eac764a8eb6cd0673555eb95
Wasif2000 <wasifali13579000@gmail.com>
2024-11-12 19:13:46 +0500

    add new buttons

--- src/components/Box1.jsx
+++ src/components/Box1.jsx
@@ -15,16 +15,27 @@ export const Box1 = () => {
         <div className='w-[45%]'>
       <h3 className='flex gap-2 pt-5 pb-[2px]'> <span className='pt-1'><LiaBullseyeSolid /></span> Cardboard Spaceship</h3>
      
-      <img src={aa} className="rounded-lg"/>
+      <img src={aa} className="rounded-lg transform transition ease-in duration-200 hover:scale-110 "/>
       </div>
       <div className='w-[45%]'>
       <h3 className='flex gap-2 pt-5 pb-[2px]'> <span className='pt-1'><LiaBullseyeSolid /></span> 
       AH2 & Matt Horn</h3>
-      <img src={bb} className="rounded-lg transform transition ease-in duration-300 hover:scale-110" 
+      <img src={bb} className="rounded-lg transform transition ease-in duration-200 hover:scale-110" 
       
       />
       </div>
       </div>
+      </div>
+      <div className='px-[70px] py-[20px]'>
+        <div className='flex gap-8'>
+          <button className='border border-black rounded-lg text-zinc-950 px-2 hover:bg-black hover:text-white uppercase'>branded template</button>
+          <button className='border border-black rounded-lg text-zinc-950 px-2 hover:bg-black hover:text-white uppercase'>sales desk</button>
+          <button className='border border-black rounded-lg text-zinc-950 px-2 hover:bg-black hover:text-white uppercase'>social media template</button>
+          <button className='border border-black rounded-lg text-zinc-950 px-2 hover:bg-black hover:text-white ml-[58px] uppercase'>pitch desk</button>
+        </div>
+      </div>
+      <div>
+        <button></button>
       </div>
     </section>
   );
