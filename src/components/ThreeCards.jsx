e813a7763529f560b60cfbbccfb5f07210525656
Wasif2000 <wasifali13579000@gmail.com>
2024-11-14 13:56:42 +0500

    add names

---
+++ src/components/ThreeCards.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ThreeCards = () => {
+  return (
+    <div>
+        <div className='flex gap-6 px-12 my-8'>
+            <div className='w-[45%] bg-[#004d43] h-[400px] rounded-xl '><h1 className='text-[#cdea68]'>ochi</h1></div>
+            <div className='w-[25%] bg-[#212121] h-[400] rounded-xl'></div>
+            <div className='w-[25%] bg-[#212121] h-[400] rounded-xl'></div>
+        </div>
+    </div>
+  )
+}
+
+export default ThreeCards
--- src/pages/SecondCard.jsx
+++ src/pages/SecondCard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Box1 } from '../components/Box1'
+import ThreeCards from '../components/ThreeCards'
 
 function SecondCard() {
   return (
     <div>
     <Box1/>
+    <ThreeCards/>
     </div>
   )
 }
